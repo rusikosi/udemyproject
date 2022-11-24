5260aa3732209a7b968ad2e9d06a143047590fcd
Rusik <rusikosi.nl@gmail.com>
2022-11-24 19:29:27 +0100

    first commit

---
+++ alert.js
@@ -0,0 +1,168 @@
+'use strict';
+
+// alert ('Привет');
+ /*alert ('Мир');*/
+
+ /*это мой первый
+ многострочный комментарий*/
+
+ //let message = 'Hello'; //сохрагить строку 'Hello' в переменной с именем message
+
+ //alert(message); //показывает содержимое переменной
+
+/*let message;
+
+ message = 'Hello!';
+
+ message = 'World!'; // значение удалено
+
+ alert(message);
+ */
+
+/*let hello = 'Hello world!';
+let message;
+// копируем значение 'Hello world' из переменной hello в переменную message
+message = hello;
+
+// теперь две переменные содержат одинаковые данные
+alert(hello);
+alert(message);*/
+
+/*let admin;
+
+let name = 'John';
+
+admin = name;
+alert(admin);
+
+const ourPlanetName = 'Earth';
+let currentUserName = 'John';
+*/
+
+
+//////
+/*
+let name = 'Иван';
+
+alert( `Привет, ${name}!`);
+alert( `результат: ${1 + 2}` );
+
+alert( "результат: ${1 + 2}" );
+*/
+
+/*
+let isGreater = 4 > 1;
+alert(isGreater); // true (результатом сравнения будет "да")
+*/
+
+/*
+let name = "Ilya";
+
+alert( `hello ${1}` );
+alert( `hello ${"name"}` );
+alert( `hello ${name}` );
+*/
+
+// result = prompt(title, [default]);
+
+/*
+
+let age = prompt('Сколько тебе лет', 100);
+
+alert(`Тебе ${age} лет!`);
+
+*/
+
+/* let isBoss = confirm('Are you a boss here?');
+alert( isBoss ); */ 
+
+
+/*let userName = prompt('What is your name', '');
+
+alert(`Your name is ${userName}!`);*/
+
+//alert('Hello');
+
+/*const result = confirm('Are you here?');
+console.log(result);
+*/
+
+//const answer = +prompt('Are you older than 18?', '18');
+//console.log(answer + 5);
+
+/*
+const answers = [];
+
+answers[0] = prompt('Как вваше имя?', '');
+answers[1] = prompt('Как вваше фамилия?', '');
+answers[2] = prompt('How old are you?', '');
+
+document.write(answers);
+
+console.log(typeof(answers));
+console.log(typeof(null));
+*/
+
+/*
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);  //`${}/прием работает только с косыми ковычками`
+
+const user = 'Ivan';
+
+alert(`Hallo, ${user}`);
+
+*/
+
+
+console.log('arr' + ' - object');
+console.log(4 + '5');
+
+let incr = 10, // оператор = это оператор присваивания (один)
+    decr = 10;
+
+    //incr++;  //оператор инкремента,те увеличение на елиницу  //постфиксная запись++
+    //decr--;  //опервтор декремента, те кменьшение на елиницу
+
+    //++incr;  //префиксная запись
+    //--decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);  // оператор % возврашает остаток после деления // 1
+
+console.log(2*4 == 8);  //если мы хотим что то сравнить, ставим == // true
+
+console.log(2*4 == '8'); // еслт сравниваем число и строку, ставим == // true
+
+console.log(2*4 === '8'); //три знака === это СТРОГОЕ сравнение // false потому что число и строка
+
+console.log(2*4 === 8); // === строгое //true число и число и сумма, все совпадает
+
+//логические операторы И и ИЛИ
+
+/*
+&& // и // работает только тогда, когда 2 или больше подопытных являются
+//правдивыми выражениями// те и ЭТО правда и ЭТО
+
+|| // или // работает когда хотя бы одно из выражений - это правда
+*/
+
+const isChecked = false,
+      isClose = false;
+
+//console.log(isChecked && isClose); false
+//console.log(isChecked || !isClose); true
+
+// ! оператор отрицания, он обращает значение в обратное
+// ! если была правда, то он обратит ее в ложь
+
+// Порядок выполнения операторов
+
+console.log(2 + 2 * 2 != 8); // !8 те не равен восьми// true
+
+
+
+
